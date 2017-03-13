52461f7f84672b433869c826b3f6077fa5f857f4
Michal Skora <mchlskora@gmail.com>
2017-03-13 11:08:53 +0100

    Webpack dev server + browser sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = env => {
-  console.log(env);
   return {
     entry: {
       'main': './src/scripts/main.js',
@@ -13,9 +13,15 @@ module.exports = env => {
     },
     output: {
       filename: '[name].js',
-      path: __dirname + '/build'
+      path: __dirname + '/build',
+      publicPath: '/build/',
     },
     devtool: env.prod ? 'eval-cheap-source-map' : 'eval',
+    bail: env.prod,
+    devServer: {
+      contentBase: 'public/',
+      inline: true,
+    },
     module: {
       loaders: [
         {
@@ -31,13 +37,13 @@ module.exports = env => {
           test: /\.(jpe?g|png|gif|ttf|woff|svg|eot)$/i,
           loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]'],
         },
-        { test: /\.json$/, loader: "json" },
+        { test: /\.json$/, loader: 'json' },
         {
           test: /\.scss$/,
           exclude: /node_modules/,
           loader: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader!sass-loader",
+            fallback: 'style-loader',
+            use: 'css-loader!sass-loader',
           }),
         },
       ]
@@ -49,6 +55,11 @@ module.exports = env => {
         'window.jQuery': 'jquery',
       }),
       new ExtractTextPlugin({ filename: 'styles.css', allChunks: true }),
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:8080/'
+      }),
     ]
   }
 };
